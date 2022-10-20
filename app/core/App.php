5fbb94fdd3498f2a7091e76acd6a0bb32065cc45
taufiq <htaufiq225@gmail.com>
2022-10-20 13:58:10 +0800

    menyiapkan mvc

--- app/core/App.php
+++ app/core/App.php
@@ -1,3 +1,19 @@
 <?php 
 
-echo "Hello World";
+class App
+{
+
+    var_dump($this->parsUrl());
+
+    public function parsUrl()
+    {
+        if(isset($_GET['url']))
+        {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+
+    }
+}
