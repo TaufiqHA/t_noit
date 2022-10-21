f55edc1a13348276cdd2cec6ed032cbf9fa39079
taufiq <htaufiq225@gmail.com>
2022-10-21 00:52:14 -0700

    menyelesaikan folder app

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,42 @@
 
 class App
 {
+    protected $controller = "home";
+    protected $method = "index";
+    protected $param = [];
 
-    var_dump($this->parsUrl());
+    public function __construct()
+    {
+        $url = $this->parsUrl();
+
+        if (!empty($url))
+        {
+            if (file_exists(__DIR__ . '/../controllers/' . $url[0] . '.php')) 
+            {
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
+        }
+
+        require_once __DIR__ . '/../controllers/' . $this->controller . ".php";
+        $this->controller = new $this->controller;
+
+        if (isset($url[1])) 
+        {
+            if (method_exists($this->controller, $url[1])) 
+            {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        if (!empty($url))
+        {
+            $this->param = array_values($url);
+        }
+
+        call_user_func_array([$this->controller, $this->method], $this->param);
+    }
 
     public function parsUrl()
     {
