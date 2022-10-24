99db60c23777d2b0b08d6951671f8d844a70900f
taufiq <htaufiq225@gmail.com>
2022-10-24 02:31:34 -0700

    menyiapkan file database

--- app/core/Database.php
+++ app/core/Database.php
@@ -2,8 +2,37 @@
 
 class Database
 {
+
+	private $dbh;
+	private $stmt;
+	private $hostname;
+	private $dbname;
+	private $user;
+	private $pass;
+
 	public function __construct()
 	{
-		
+		$dsn = "mysql:host = $this->hostname;dbname = $this->dbname";
+
+		$option = [
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRMODE => PDF::ERRMODE_EXCEPTION
+		];
+
+		try {
+			$dbh = new PDO($dsn, $this->user, $this->pass, $option);
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+
+		public function query($query)
+		{
+			$this->stmt = $dbh->prepare($query);
+		}
+
+		public function bind($param, $value, $type = null)
+		{
+			
+		}
 	}
 }
