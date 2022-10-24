54f6366193681335294bed7d5c9d4ac22c8dc458
TaufiqHA <htaufiq225@gmail.com>
2022-10-24 20:53:09 +0700

    menyelesaikan database dan menambahkan file config

--- app/core/Database.php
+++ app/core/Database.php
@@ -32,7 +32,40 @@ class Database
 
 		public function bind($param, $value, $type = null)
 		{
-			
+			if (is_null($type)) {
+				switch (true) {
+					case is_int($value):
+						$type = PDO::PARAM_INT;
+						break;
+					case is_bool($value):
+						$type = PDO::PARAM_BOOL;
+						break;
+					case is_null($value):
+						$type = PDO::PARAM_NULL;
+						break;
+					
+					default:
+						$type = PARAM_STR;
+						break;
+				}
+			}
+			$this->stmt->bindValue($param , $value, $type);
+		}
+		public function execute()
+		{
+			$this->stmt->execute();
+		}
+
+		public function all()
+		{
+			$this->execute();
+		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+
+		public function single()
+		{
+			$this->execute();
+			return $this->stmt->fetch(PDO::FETCH_ASSOC);
 		}
 	}
 }
