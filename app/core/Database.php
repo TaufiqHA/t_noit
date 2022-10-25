65b69d91a5f1c4365d15faa884574c6a59e99945
TaufiqHA <htaufiq225@gmail.com>
2022-10-25 08:22:29 +0700

    ubah file database

--- app/core/Database.php
+++ app/core/Database.php
@@ -24,48 +24,48 @@ class Database
 		} catch (Exception $e) {
 			die($e->getMessage());
 		}
+	}
 
-		public function query($query)
+	public function query($query)
 		{
 			$this->stmt = $dbh->prepare($query);
 		}
 
-		public function bind($param, $value, $type = null)
-		{
-			if (is_null($type)) {
-				switch (true) {
-					case is_int($value):
-						$type = PDO::PARAM_INT;
-						break;
-					case is_bool($value):
-						$type = PDO::PARAM_BOOL;
-						break;
-					case is_null($value):
-						$type = PDO::PARAM_NULL;
-						break;
-					
-					default:
-						$type = PARAM_STR;
-						break;
-				}
+	public function bind($param, $value, $type = null)
+	{
+		if (is_null($type)) {
+			switch (true) {
+				case is_int($value):
+					$type = PDO::PARAM_INT;
+					break;
+				case is_bool($value):
+					$type = PDO::PARAM_BOOL;
+					break;
+				case is_null($value):
+					$type = PDO::PARAM_NULL;
+					break;
+				
+				default:
+					$type = PARAM_STR;
+					break;
 			}
-			$this->stmt->bindValue($param , $value, $type);
 		}
-		public function execute()
-		{
-			$this->stmt->execute();
-		}
+		$this->stmt->bindValue($param , $value, $type);
+	}
+	public function execute()
+	{
+		$this->stmt->execute();
+	}
 
-		public function all()
-		{
-			$this->execute();
-		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
-		}
+	public function all()
+	{
+		$this->execute();
+	return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
 
-		public function single()
-		{
-			$this->execute();
-			return $this->stmt->fetch(PDO::FETCH_ASSOC);
-		}
+	public function single()
+	{
+		$this->execute();
+		return $this->stmt->fetch(PDO::FETCH_ASSOC);
 	}
 }
